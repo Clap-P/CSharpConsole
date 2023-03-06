49a1ee898a4d3ec636c3d68c6cc72ce378013e94
PierreC <pierre.clapisson@viacesi.fr>
2023-03-06 12:09:42 +0100

    feat

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+      //  int number = 10;
+       // string name = "Rollon Normand";
+       // double percentage = 10.23;
+       // char gender = 'M';
+       // bool isVerified = true;
+        //Console.WriteLine("Id: " + number);
+        //Console.WriteLine("Name: " + name);
+        //Console.WriteLine("Percentage " + percentage);
+        //Console.WriteLine("Gender " + gender);
+        //Console.WriteLine("Verified " + isVerified);
+        //Console.ReadLine();
+
+
+        //Fonction 2
+        double salary = 25000;
+        double tax = 30;
+        Console.WriteLine(salary + " " + tax);
+        salary = Taxes(salary, tax);
+        Console.WriteLine(salary + " " + tax);
+        Console.WriteLine("PressEnterKeyToExit");
+        Console.ReadLine();
+    }
+    static double Taxes(double salary, double tax)
+    {
+        return (salary * (1-tax/100));
+    }
+
+}
