d382558360c7c777ded61dbff22f8699dd33bc94
PierreC <pierre.clapisson@viacesi.fr>
2023-03-07 10:17:09 +0100

    Final Feats

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,28 +1,22 @@
 ﻿class Program
 {
-
     static void Main(string[] args)
     {
-        ////////////////  int number = 10;
-        //////////////// string name = "Rollon Normand";
-        //////////////// double percentage = 10.23;
-        //////////////// char gender = 'M';
-        //////////////// bool isVerified = true;
-        ////////////////Console.WriteLine("Id: " + number);
-        ////////////////Console.WriteLine("Name: " + name);
-        ////////////////Console.WriteLine("Percentage " + percentage);
-        //Console.WriteLine("Gender " + gender);
-        //Console.WriteLine("Verified " + isVerified);
-        //Console.ReadLine();
+        int number = 10;
+        string name = "Rollon Normand";
+        double percentage = 10.23;
+        char gender = 'M';
+        bool isVerified = true;
+        Console.WriteLine("Id: " + number);
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Percentage: " + percentage);
+        Console.WriteLine("Gender: " + gender);
+        Console.WriteLine("Verified: " + isVerified);
 
-
-        //Fonction 2
-
-        string[] Month = new string[] { "Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre" };
-        Console.WriteLine("Exercice 2\n\n");
+        // Exercice 2
+        string[] Months = new string[] { "Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Decembre" };
+        Console.WriteLine("\nExercice 2\n\n");
         Console.Write("Salaire : ");
-
-
 
         bool salaryIsDouble = Double.TryParse(Console.ReadLine(), out double salary);
         if (salaryIsDouble)
@@ -41,14 +35,13 @@
                 default:
                     break;
             }
-        
         }
         else
         {
             Console.WriteLine("Le Salaire n'est pas une donnée");
         }
         Console.Write("Taxe : ");
-        //Vérification taxe
+        // Vérification taxe
         bool taxIsDouble = Double.TryParse(Console.ReadLine(), out double tax);
         if (taxIsDouble)
         {
@@ -59,35 +52,52 @@
             Console.WriteLine("La taxe n'est pas une donnée");
         }
 
+        Console.WriteLine("---------------------------------------------");
+        Console.WriteLine("Salaire Brut : " + salary + " euros\nTaxe : " + tax + " %");
+        Console.WriteLine("");
+
+        double salaireNet = 0;
         for (int i = 0; i < 12; i++)
         {
+            // Aout
             if (i == 7)
             {
                 Console.Write("");
             }
+            // Decembre
             else if (i == 11)
             {
-                Console.WriteLine(Month[i] + " : " + ((salary / 12) + ((salary * 10) / 100)));
+                Console.Write("Prime de Décembre : ");
+                bool bonusIsDouble = Double.TryParse(Console.ReadLine(), out double bonus);
+                try
+                {
+                    Console.WriteLine(Months[i] + " : " + ((salary / 11) + ((salary * bonus) / 100)));
+                    salaireNet = salaireNet + (((salary / 11) + ((salary * bonus) / 100)));
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Le format de la prime est mauvais");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("La prime vaut 0");
+                }
             }
             else
             {
-                Console.WriteLine(Month[i] + " : " + (salary / 12));
+                Console.WriteLine(Months[i] + " : " + (salary / 11));
+                salaireNet = salaireNet + (salary / 11);
             }
         }
 
-        Console.WriteLine("Salaire Brut : " + salary + " Euros\nTaxe : " + tax + " %");
-        salary = Taxes(salary, tax);
-        Console.WriteLine("Salaire Net : " + salary + " Euros");
+        Console.WriteLine("\nSalaire Net : " + Taxes(salary, tax) + " Euros");
         Console.WriteLine("---------------------------------------------");
-        Console.WriteLine("\nPressEnterKeyToExit");
+        Console.WriteLine("\nPress Enter Key To Exit");
         Console.ReadLine();
     }
 
-    
-
     static double Taxes(double salary, double tax)
     {
-        return (salary * (1-tax/100));
+        return (salary * (1 - tax / 100));
     }
-    //a
 }
